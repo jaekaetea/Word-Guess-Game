09c507bb5ec82170e8b4cc265182a1aaaaf243eb
JoJo <jae.kae.tea@gmail.com>
2019-06-10 08:59:29 -0500

    Added style, hint, different header

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,12 @@
 var punkBands = ["BLINK-182", "GREEN DAY", "MY CHEMICAL ROMANCE", "FALL OUT BOY", "SUM 41",
 "JIMMY EAT WORLD", "SIMPLE PLAN", "THE ALL-AMERICAN REJECTS", "THE OFFSPRING", "RADIOHEAD", 
-"THE STROKES", "ARCADE FIRE", "ARCTIC MONKEYS", "THE KILLERS", "INTERPOL", 
-"GORILLAZ", "DEATH CAB FOR CUTIE", "EVANESCENCE", "BOWLING FOR SOUP", "NEW FOUND GLORY"];
+"THE STROKES", "ARCADE FIRE", "ARCTIC MONKEYS", "THE KILLERS", "INTERPOL", "GORILLAZ", 
+"EVANESCENCE", "BOWLING FOR SOUP", "NEW FOUND GLORY"];
+
+//no doubt - running, the redjump suit - push you down
+//alkaline trio mercy me, sugarcult memory
+//brand new -sic , relient k be escape
+//against me trash unreal, weezer hash pipe
 
 var numGuesses = 10;
 var guessesLeft = 0;
@@ -13,16 +18,11 @@ var losses = 0;
 var isFinished = false;
 
 BandPicture = document.getElementById("bandPic");
+var mySound = document.getElementById("keyboardcat");
 
 confirm("Press OK if you are 18 years or older.  If not, please get parental approval.");
 
-var mySound = document.getElementById("keyboardcat");
-
-function playAudio(x) {
-  mySound = document.getElementById(x); 
-  mySound.currentTime = 0;  
-  mySound.play();  
-}
+$("#loser").text("PRESS ANY KEY TO GET STARTED!");
 
 function setUp() {
   mySound.pause(); 
@@ -41,6 +41,8 @@ function setUp() {
   }
 
   guessesLeft = numGuesses;
+  document.getElementById("numGuesses").style.color = "#ffffff";
+  document.getElementById("numGuesses").style.fontSize = "initial";
   guessedLetters =[];
 
   $("#loser").text("");
@@ -49,6 +51,7 @@ function setUp() {
 
   console.log(answer);
   console.log(answerArray);
+
   updates();
 }
 
@@ -65,10 +68,18 @@ function check(letter) {
       guessedLetters.push(letter);
       if (answer.indexOf(letter) === -1) {
           guessesLeft--;
-          //if (guessesLeft <=3) {
-           // document.getElementById("numGuesses").style.color = "#be0000";
-
-          //}
+          if (guessesLeft <=3) {
+            if(guessesLeft === 3) {
+              document.getElementById("numGuesses").style.color = "#be0000";
+              document.getElementById("numGuesses").style.fontSize = "large";
+              } else if (guessesLeft === 2) {
+                document.getElementById("numGuesses").style.fontSize = "x-large";
+              } else if (guessesLeft === 1) {
+                document.getElementById("numGuesses").style.fontSize = "xx-large";
+              }           
+            setUp2();
+            $("#loser").text("Here's a Hint!");
+          }
       } else { 
           for (var x = 0; x < answer.length; x++) {
               if (letter === answer[x]) {
@@ -77,8 +88,12 @@ function check(letter) {
           }                
       }
   }
+}
 
-}; 
+function getAudio(x) {
+  mySound = document.getElementById(x); 
+  mySound.currentTime = 0;  
+}
 
 function isLoser() {
   if (guessesLeft <= 0) {
@@ -89,7 +104,8 @@ function isLoser() {
     var loserPrompt = "Please press ANY key to try again!";
     $("#loser").text(loserPrompt);
     document.getElementById("bandPic").src = "https://media.giphy.com/media/SEO7ub2q1fORa/giphy.gif";
-    playAudio("keyboardcat");
+    getAudio("keyboardcat");
+    mySound.play();
   }
 }
 
@@ -99,68 +115,10 @@ function isWinner(x) {
     updates();
     isFinished = true;
     //add pictures and music here
+    setUp2();
+    $("#loser").text("");
     document.getElementById("bandName").textContent = answer;
-    if(answer === "BLINK-182") {
-        BandPicture.src = 'assets/images/blink182.jpg';
-        playAudio("blink182");
-      } else if (answer === "GREEN DAY") {
-        BandPicture.src = 'assets/images/greenday.jpg';
-        playAudio("greenday");
-      } else if (answer === "MY CHEMICAL ROMANCE") {
-        BandPicture.src = 'assets/images/mychemicalromance.jpg';
-        playAudio("mychemicalromance");
-      } else if (answer === "FALL OUT BOY") {
-        BandPicture.src = 'assets/images/falloutboy.jpg';
-        playAudio("falloutboy");
-      } else if (answer === "SUM 41") {
-        BandPicture.src = 'assets/images/sum41.jpg';
-        playAudio("sum41");
-      } else if (answer === "JIMMY EAT WORLD") {
-        BandPicture.src = 'assets/images/jimmyeatworld.jpg';
-        playAudio("jimmyeatworld");
-      } else if (answer === "SIMPLE PLAN") {
-        BandPicture.src = 'assets/images/simpleplan.jpg';
-        playAudio("simpleplan");
-      } else if (answer === "THE ALL-AMERICAN REJECTS") {
-        BandPicture.src = 'assets/images/theallamericanrejects.jpg';
-        playAudio("theallamericanrejects");
-      } else if (answer === "THE OFFSPRING") {
-        BandPicture.src = 'assets/images/theoffspring.jpg';
-        playAudio("theoffspring");
-      } else if (answer === "RADIOHEAD") {
-        BandPicture.src = 'assets/images/radiohead.jpg';
-        playAudio("radiohead");
-      } else if (answer === "THE STROKES") {
-        BandPicture.src = 'assets/images/thestrokes.jpg';
-        playAudio("thestrokes");
-      } else if (answer === "ARCADE FIRE") {
-        BandPicture.src = 'assets/images/arcadefire.jpg';
-        playAudio("arcadefire");
-      } else if (answer === "ARCTIC MONKEYS") {
-        BandPicture.src = 'assets/images/arcticmonkeys.jpg';
-        playAudio("arcticmonkeys");
-      } else if (answer === "THE KILLERS") {
-        BandPicture.src = 'assets/images/thekillers.jpg';
-        playAudio("thekillers");
-      } else if (answer === "INTERPOL") {
-        BandPicture.src = 'assets/images/interpol.jpg';
-        playAudio("interpol");
-      } else if (answer === "GORILLAZ") {
-        BandPicture.src = 'assets/images/gorillaz.jpg';
-        playAudio("gorillaz");
-      } else if (answer === "DEATH CAB FOR CUTIE") {
-        BandPicture.src = 'assets/images/deathcabforcutie.jpeg';
-        playAudio("deathcabforcutie");
-      } else if (answer === "EVANESCENCE") {
-        BandPicture.src = 'assets/images/evanescence.jpg';
-        playAudio("evanescence");
-      } else if (answer === "BOWLING FOR SOUP") {
-        BandPicture.src = 'assets/images/bowlingforsoup.jpg';
-        playAudio("bowlingforsoup");
-      } else if (answer === "NEW FOUND GLORY") {
-        BandPicture.src = 'assets/images/newfoundglory.jpg';
-        playAudio("newfoundglory");
-      }
+    mySound.play()
     }
 }
 
@@ -176,6 +134,67 @@ document.onkeyup = function(event) {
       isWinner(answer);
       isLoser();
     }
+  }
+}
+
+function setUp2() {
+  if(answer === "BLINK-182") {
+    BandPicture.src = 'assets/images/blink182.jpg';
+    getAudio("blink182");
+  } else if (answer === "GREEN DAY") {
+    BandPicture.src = 'assets/images/greenday.jpg';
+    getAudio("greenday");
+  } else if (answer === "MY CHEMICAL ROMANCE") {
+    BandPicture.src = 'assets/images/mychemicalromance.jpg';
+    getAudio("mychemicalromance");
+  } else if (answer === "FALL OUT BOY") {
+    BandPicture.src = 'assets/images/falloutboy.jpg';
+    getAudio("falloutboy");
+  } else if (answer === "SUM 41") {
+    BandPicture.src = 'assets/images/sum41.jpg';
+    getAudio("sum41");
+  } else if (answer === "JIMMY EAT WORLD") {
+    BandPicture.src = 'assets/images/jimmyeatworld.jpg';
+    getAudio("jimmyeatworld");
+  } else if (answer === "SIMPLE PLAN") {
+    BandPicture.src = 'assets/images/simpleplan.jpg';
+    getAudio("simpleplan");
+  } else if (answer === "THE ALL-AMERICAN REJECTS") {
+    BandPicture.src = 'assets/images/theallamericanrejects.jpg';
+    getAudio("theallamericanrejects");
+  } else if (answer === "THE OFFSPRING") {
+    BandPicture.src = 'assets/images/theoffspring.jpg';
+    getAudio("theoffspring");
+  } else if (answer === "RADIOHEAD") {
+    BandPicture.src = 'assets/images/radiohead.jpg';
+    getAudio("radiohead");
+  } else if (answer === "THE STROKES") {
+    BandPicture.src = 'assets/images/thestrokes.jpg';
+    getAudio("thestrokes");
+  } else if (answer === "ARCADE FIRE") {
+    BandPicture.src = 'assets/images/arcadefire.jpg';
+    getAudio("arcadefire");
+  } else if (answer === "ARCTIC MONKEYS") {
+    BandPicture.src = 'assets/images/arcticmonkeys.jpg';
+    getAudio("arcticmonkeys");
+  } else if (answer === "THE KILLERS") {
+    BandPicture.src = 'assets/images/thekillers.jpg';
+    getAudio("thekillers");
+  } else if (answer === "INTERPOL") {
+    BandPicture.src = 'assets/images/interpol.jpg';
+    getAudio("interpol");
+  } else if (answer === "GORILLAZ") {
+    BandPicture.src = 'assets/images/gorillaz.jpg';
+    getAudio("gorillaz");
+  } else if (answer === "EVANESCENCE") {
+    BandPicture.src = 'assets/images/evanescence.jpg';
+    getAudio("evanescence");
+  } else if (answer === "BOWLING FOR SOUP") {
+    BandPicture.src = 'assets/images/bowlingforsoup.jpg';
+    getAudio("bowlingforsoup");
+  } else if (answer === "NEW FOUND GLORY") {
+    BandPicture.src = 'assets/images/newfoundglory.jpg';
+    getAudio("newfoundglory");
   }
 }
 
